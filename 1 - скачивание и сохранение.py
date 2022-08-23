595586bbaa76f2c6962c48af73be5e6128c7250b
mkoninin <42993207+mkoninin@users.noreply.github.com>
2022-08-23 19:26:54 +0700

    Initial commit

---
+++ 1 - скачивание и сохранение.py
@@ -0,0 +1,18 @@
+﻿import json, urllib.request
+import base64
+
+import datetime
+
+date = str(datetime.date.today())
+
+periods = ['week', 'month', '3month']
+
+for period in periods:
+    print (f'downloading ... {period}')
+    js = json.load(urllib.request.urlopen(f'https://trending-searches.wb.ru/file?period={period}'))
+    if (js['error'] == False):
+        data = base64.b64decode(js['data']['file'])
+        fp = open(f"data/wbqueries-{period}-{date}.csv", "wb")
+        fp.write(data)
+    else:
+        print (f'ERROR downloading ... {period}')
